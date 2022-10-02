b6019c96553cfeea94dd798abec3b6fa5eb3e0d6
Mohammed Zubair <moh.775123554@gmail.com>
2022-10-02 16:03:26 +0300

    complete localization
    complete localization with middleware

---
+++ app/Http/Controllers/Api/v1/LocalizationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LocalizationRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+class LocalizationController extends Controller
+{
+    public function __invoke(LocalizationRequest $request)
+    {
+       $validated_data=$request->validated();
+     App::setlocale($validated_data['lang']); //change locale app
+     session()->put('local',$validated_data['lang']) ;// make session with key local and value the request from lang
+     return successResponse(['currentLanguage'=>App::getLocale()=="en"?__('language.en'):__('language.ar')],__('response.update.success'),201);
+    }
+}
